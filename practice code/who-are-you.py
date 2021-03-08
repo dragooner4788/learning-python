62aafb8f9a6dc2193b768347f0b3abaf2c62cf75
Guy Friley <gfriley@hotmail.com>
2021-03-07 23:52:07 -0500

    Using dictionary to store information about a user

---
+++ practice code/who-are-you.py
@@ -0,0 +1,14 @@
+profile = {
+    "first name": "",
+    "last name": ""
+}
+
+user_first_name = input("What is your first name? ")
+
+user_last_name = input("What is your last name? ")
+
+profile["first name"] = user_first_name
+
+profile["last name"] = user_last_name
+
+print(profile)
