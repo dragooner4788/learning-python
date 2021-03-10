41cbf15a0d93f065e92102592ec62fc123e5b74a
Guy Friley <gfriley@hotmail.com>
2021-03-09 23:08:20 -0500

    Getting risetimes and formatting the output

--- astronauts/astronauts.py
+++ astronauts/astronauts.py
@@ -1,6 +1,15 @@
+'''
+Description: using the Open Notify system which we can gather details about the international Space Station
+Why: I am doing this to learn more about APIs and how to use them in Python. 
+Date: 2/9/2021 10:43PM
+'''
+
+
 import requests
 import json
 
+
+# Getting information about the astronauts on the ISS right now.
 response = requests.get("http://api.open-notify.org/astros.json")
 
 
@@ -11,3 +20,26 @@ def jprint(obj):
 
 
 jprint(response.json())
+
+
+location_iss_parameters = {  # parameters to pass to the request we are making.
+    "lat": 40.4406,
+    "lon": 79.9959
+}
+location_iss = requests.get(
+    # request made to open notify.
+    "http://api.open-notify.org/iss-pass.json", params=location_iss_parameters
+)
+
+# We are just getting the response key from the dictionary/json we get back.
+pass_times = location_iss.json()['response']
+jprint(pass_times)
+
+risetimes = []
+
+for risestime in pass_times:
+    time = risestime['risetime']
+    risetimes.append(time)
+
+for risetime in risetimes:
+    print(f"Rise Time: {risestime['risetime']}")
