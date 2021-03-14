900c258dc0bdf506ba4ff179e655aeb2aae27219
Guy Friley <gfriley@hotmail.com>
2021-03-14 00:07:39 -0500

    inviting a list of guests from a list

---
+++ Python Crash Course/guest-list.py
@@ -0,0 +1,10 @@
+'''
+Name: Guy F.
+Date: 3/14/2021 12:04am
+Description: Create a list of people living or dead and then invite them to your party.
+'''
+
+guests = ["Steve Jobs", "Bill Gates", "Scott Galloway"]
+
+for guest in guests:
+    print(f"Hello, {guest}! I am inviting you to my party!")
