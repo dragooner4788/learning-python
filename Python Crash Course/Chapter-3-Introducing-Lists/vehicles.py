69493b32ade87e3a4ee89dc37fd694a7d9f2b844
Guy Friley <gfriley@hotmail.com>
2021-03-13 21:11:31 -0500

    create message with vehicles in it using a list

---
+++ Python Crash Course/Chapter-3-Introducing-Lists/vehicles.py
@@ -0,0 +1,14 @@
+'''
+Name: Guy Friley
+Date: 3/13/2021
+Description: Create a list of vehicles and create statements about them.
+
+'''
+
+vehicles = ["Honda Civic", "Hyundai Elantra", "Ford Mustang"]
+
+message = f"I currently own a {vehicles[1]} and it works great! The new {vehicles[0]} look cool. A {vehicles[2]} guzzles a lot of gas!"
+
+print(
+    message
+)
