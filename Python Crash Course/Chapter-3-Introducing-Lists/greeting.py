b9bc5e95cc036a07679077e3205b399cd58215cd
Guy Friley <gfriley@hotmail.com>
2021-03-13 21:04:44 -0500

    Exercises from PCC Chapter 3

---
+++ Python Crash Course/Chapter-3-Introducing-Lists/greeting.py
@@ -0,0 +1,19 @@
+'''
+Name: Guy Friley
+Date: 3/13/2021
+Description: Getting a name from a list and printing a greeting
+'''
+
+name_of_friends = ["jessica", "taylor", "josephine"]
+
+message_jessica = f"Hello, {name_of_friends[0].title()}!"
+
+message_taylor = f"Hello, {name_of_friends[1].title()}!"
+
+message_josephine = f"Hello, {name_of_friends[2].title()}!"
+
+print(message_jessica)
+
+print(message_taylor)
+
+print(message_josephine)
---
+++ Python Crash Course/Chapter-3-Introducing-Lists/list-names.py
@@ -0,0 +1,11 @@
+'''
+Name: Guy Friley
+Date: 3/13/2021 8:56PM
+Description: Create a list of names and print them out
+'''
+
+names_of_friends = ["John", "Ted", "Bill"]
+
+print(names_of_friends[0] + "\n" +
+      names_of_friends[1] + "\n" +
+      names_of_friends[2])
