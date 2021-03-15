2a3a612116be7e380ac0918f87d83b69ad817e3d
Guy Friley <gfriley@hotmail.com>
2021-03-14 20:56:53 -0400

    using the sorting methods and functions for lists

---
+++ Python Crash Course/Chapter-3-Introducing-Lists/places-to-visit.py
@@ -0,0 +1,33 @@
+'''
+Name: Guy F.
+Date: 3/14/2021
+Description: Using the sorting methods for lists and getting practice.
+'''
+
+places_to_visit = ["japan", "australia", "france",
+                   "new zealand", "south korea", "canada"]
+
+print(places_to_visit)
+
+print(sorted(places_to_visit))
+
+print(places_to_visit)
+
+print(sorted(places_to_visit, reverse=True))
+
+print(places_to_visit)
+
+places_to_visit.reverse()
+print(places_to_visit)
+
+places_to_visit.reverse()
+print(places_to_visit)
+
+places_to_visit.sort()
+print(places_to_visit)
+places_to_visit.sort(reverse=True)
+print(places_to_visit)
+
+# don't feel like going back to previous homework so will do the exercise here
+
+print(len(places_to_visit))
