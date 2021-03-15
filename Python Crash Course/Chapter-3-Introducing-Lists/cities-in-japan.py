722549b3bb670f7399ef614d130514c0dd15ebe3
Guy Friley <gfriley@hotmail.com>
2021-03-14 21:02:00 -0400

    exercise using functions and methods

---
+++ Python Crash Course/Chapter-3-Introducing-Lists/cities-in-japan.py
@@ -0,0 +1,28 @@
+cities_in_japan = ["tokyo", "osaka", "sendai",
+                   "kyoto", "hiroshima", "nagasaki"]
+
+cities_in_japan.sort()
+
+print(cities_in_japan)
+
+cities_in_japan.reverse()
+
+print(cities_in_japan)
+
+removed_city = cities_in_japan.pop(-1)
+
+print(removed_city)
+
+print(cities_in_japan)
+
+cities_in_japan.sort(reverse=True)
+
+print(cities_in_japan)
+
+reversed(cities_in_japan)
+
+print(cities_in_japan)
+
+sorted(cities_in_japan)
+
+print(cities_in_japan)
