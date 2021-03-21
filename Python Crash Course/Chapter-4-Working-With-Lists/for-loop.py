89e7064441f4bd6ea5b86df070d0e4b91b297c4f
Guy Friley <gfriley@hotmail.com>
2021-03-21 16:06:57 -0400

    using list comprehensions and for loops

---
+++ Python Crash Course/Chapter-4-Working-With-Lists/for-loop.py
@@ -0,0 +1,11 @@
+#numbers_1_20 = [print(number) for number in range(1, 21)]
+
+#numbers_1_1000000 = [print(number) for number in range(1, 1_000_000)]
+
+numbers = list(range(1, 1_000_001))
+
+print(min(numbers))
+
+print(max(numbers))
+
+print(sum(numbers))
