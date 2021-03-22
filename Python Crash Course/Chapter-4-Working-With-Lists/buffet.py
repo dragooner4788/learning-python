e5dc453bacafc8b2a3bd2163481472ddcde96270
Guy Friley <gfriley@hotmail.com>
2021-03-21 22:49:06 -0400

    Working with Tuples.

---
+++ Python Crash Course/Chapter-4-Working-With-Lists/buffet.py
@@ -0,0 +1,15 @@
+'''
+Name: Guy F.
+Date: 3-21-2021
+Description: Working with Tuples
+'''
+
+food_items = ("Chicken", "Beef", "Salad", "Potatoes", "French Fries")
+
+for food in food_items:
+    print(food)
+
+food_items = ("Sushi", "Okonomiyaki", "Ramen", "Steamed Buns", "Mochi")
+
+for food in food_items:
+    print(food)
